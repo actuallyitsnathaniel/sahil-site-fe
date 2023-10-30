8f0e7fda9562f47d0173c5c55314de995230f5fc
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-10-29 20:40:29 -0700

    refactored styling

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,18 +4,12 @@ export const Header = () => {
   return (
     <div id="header" className="absolute w-screen flex flex-col -mt-10">
       <SahilLogo />
-      <div className="flex-row justify-between py-3 md:py-10 text-center font-light animate-appear-slow ">
-        <span className="">
-          Composer
-        </span>
-        <span className="px-2 md:px-5">|</span>
-        <span className="">
-          Orchestrator
-        </span>
-        <span className="px-2 md:px-5">|</span>
-        <span className="">
-          Music Editor
-        </span>
+      <div className="flex-row justify-between py-3 md:py-10 text-center font-light animate-appear-slow [&>*]:px-2 [&>*:md]:px-2.5">
+        <span>Composer</span>
+        <span>|</span>
+        <span>Orchestrator</span>
+        <span>|</span>
+        <span>Music Editor</span>
       </div>
     </div>
   );
