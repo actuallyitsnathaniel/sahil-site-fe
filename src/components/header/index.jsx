97a04901ceb3d2fccbee8aa703282999b28acac1
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-10-29 20:33:39 -0700

    oops

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,7 @@ import { SahilLogo } from "../logo-component";
 
 export const Header = () => {
   return (
-    <div id="header" className="absolute w-screen flex flex-col -mt-24center">
+    <div id="header" className="absolute w-screen flex flex-col -mt-24">
       <SahilLogo />
       <div className="flex-row justify-between py-10 text-center text-md font-light animate-appear-slow ">
         <span className="">
