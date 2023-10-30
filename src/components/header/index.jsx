b904cb435ca99e8104f15f136121d3cc099842bf
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-10-29 20:33:27 -0700

    updated header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,8 +2,21 @@ import { SahilLogo } from "../logo-component";
 
 export const Header = () => {
   return (
-    <div id="header" className="absolute w-screen flex flex-col -mt-24">
+    <div id="header" className="absolute w-screen flex flex-col -mt-24center">
       <SahilLogo />
+      <div className="flex-row justify-between py-10 text-center text-md font-light animate-appear-slow ">
+        <span className="">
+          Composer
+        </span>
+        <span className="px-5">|</span>
+        <span className="">
+          Orchestrator
+        </span>
+        <span className="px-5">|</span>
+        <span className="">
+          Music Editor
+        </span>
+      </div>
     </div>
   );
 };
