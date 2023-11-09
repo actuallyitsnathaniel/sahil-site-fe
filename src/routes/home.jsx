eef7d90d2afa4ec74d5447f04430dbcd941e7c6b
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-11-08 19:00:31 -0800

    fixing

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
   return (
     <div
       id="home"
-      className="flex flex-wrap flex-col min-h-screen justify-center md:pt-16"
+      className="flex flex-col w-screen min-h-screen justify-center md:pt-16"
     >
       <Header />
     </div>
