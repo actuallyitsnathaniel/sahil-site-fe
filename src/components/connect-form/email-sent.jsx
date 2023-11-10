64b9dbe146b10b72aef5ab5f224093d61aae5235
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-11-09 18:28:28 -0800

    TODO: connect to email service

--- src/components/connect-form/email-sent.jsx
+++ src/components/connect-form/email-sent.jsx
@@ -3,9 +3,11 @@ import { PropTypes } from "prop-types";
 export const EmailSent = (props) => {
   return (
     <div className="flex justify-center text-3xl text-center max-h-screen">
-      Thanks for connecting,&#xa0;
-      <span className="first-letter:uppercase">{props.firstName}</span>.
-      I&apos;ll reach out soon!
+      <p>
+        Thanks for connecting,&#xa0;
+        <span className="first-letter:uppercase">{props.firstName}</span>.
+        I&apos;ll reach out soon!
+      </p>
     </div>
   );
 };
--- src/routes/connect.jsx
+++ src/routes/connect.jsx
@@ -4,7 +4,7 @@ const Connect = () => {
   return (
     <div
       id="connect"
-      className="flex flex-wrap flex-row h-[90vh] my-auto justify-center md:pt-14"
+      className="flex flex-wrap flex-row min-h-[90vh] my-auto justify-center md:pt-14"
     >
       <h2 className="sticky top-1.5 md:hidden py-5 text-4xl text-center">
         Connect
