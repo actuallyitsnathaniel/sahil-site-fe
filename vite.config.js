250dabcc1a4858744dd55b6f8270d47b206f7566
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-11-10 14:33:17 -0800

    config update

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,33 @@
-import { defineConfig } from "vite";
+/* eslint-disable no-undef */
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  server: {
-    open: true,
-    port: 3000,
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env.VITE_EMAILJS_SERVICE_ID": JSON.stringify(
+        env.VITE_EMAILJS_SERVICE_ID
+      ),
+      "process.env.VITE_EMAILJS_TEMPLATE_ID": JSON.stringify(
+        env.VITE_EMAILJS_TEMPLATE_ID
+      ),
+      "process.env.VITE_EMAILJS_PUBLIC_KEY": JSON.stringify(
+        env.VITE_EMAILJS_PUBLIC_KEY
+      ),
     },
-    outDir: "./build",
-  },
+    plugins: [react(), svgr()],
+    server: {
+      open: true,
+      port: 3000,
+    },
+    build: {
+      commonjsOptions: {
+        transformMixedEsModules: false,
+      },
+      outDir: "./build",
+    },
+  };
 });
