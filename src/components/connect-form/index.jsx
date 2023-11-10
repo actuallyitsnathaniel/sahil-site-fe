dec780fb6b768ad05e8d31718947c1a24512944a
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-11-10 11:50:11 -0800

    cleaned up form and ensured values were properly declared

--- src/components/connect-form/index.jsx
+++ src/components/connect-form/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { EmailSent } from "./email-sent";
 
 export const ConnectForm = () => {
@@ -7,6 +7,8 @@ export const ConnectForm = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [submitted, setSubmitted] = useState(false);
+
+  const form = useRef();
 
   const inputStyle =
     "flex mx-auto justify-between m-2 bg-opacity-20 rounded-md bg-white p-2 font-normal";
@@ -17,6 +19,7 @@ export const ConnectForm = () => {
   const HandleSubmit = (e) => {
     console.log("handleSubmit ran");
     e.preventDefault();
+    console.log(form.current);
     if (firstName != "" && lastName != "" && email != "" && message != "") {
       setSubmitted(true);
     }
@@ -32,63 +35,73 @@ export const ConnectForm = () => {
   };
 
   return (
-    <form id="connect-form" className="flex flex-wrap flex-row items-center">
+    <div className="flex flex-wrap flex-row">
       {!submitted ? (
-        <div id="input-wrapper" className="flex flex-col justify-items-center">
-          <h2 className="text-4xl p-6 text-center">
-            Let&apos;s Make Something
-          </h2>
-          <div className="md:flex md:flex-row md:space-x-4">
-            <input
-              type="text"
-              id="input-firstName"
+        <form
+          ref={form}
+          id="connect-form"
+          className="flex flex-wrap flex-row items-center"
+        >
+          <div
+            id="input-wrapper"
+            className="flex flex-col justify-items-center"
+          >
+            <h2 className="text-4xl p-6 text-center">
+              Let&apos;s Make Something
+            </h2>
+            <div className="md:flex md:flex-row md:space-x-4">
+              <input
+                type="text"
+                id="input-firstName"
+                className={`${inputStyle} ${focusClasses} ${placeholderClasses}`}
+                placeholder="first name"
+                value={firstName}
+                onChange={(e) => {
+                  setFirstName(e.target.value);
+                }}
+              />
+              <input
+                type="text"
+                id="input-lastName"
+                className={`${inputStyle} ${focusClasses} ${placeholderClasses}`}
+                placeholder="last name"
+                value={lastName}
+                onChange={(e) => {
+                  setLastName(e.target.value);
+                }}
+              />
+              <input
+                type="email"
+                id="input-email"
+                className={`${inputStyle} ${focusClasses} ${placeholderClasses}`}
+                placeholder="email@mail.com"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <textarea
+              id="message"
+              className={`${inputStyle} ${focusClasses} ${placeholderClasses} pb-28 md:w-full`}
+              placeholder="message"
               onChange={(e) => {
-                setFirstName(e.target.value);
+                setMessage(e.target.value);
               }}
-              className={`${inputStyle} ${focusClasses} ${placeholderClasses}`}
-              placeholder="first name"
             />
             <input
-              type="text"
-              id="input-lastName"
-              className={`${inputStyle} ${focusClasses} ${placeholderClasses}`}
-              placeholder="last name"
-              onChange={(e) => {
-                setLastName(e.target.value);
-              }}
-            />
-            <input
-              type="email"
-              id="input-email"
-              className={`${inputStyle} ${focusClasses} ${placeholderClasses}`}
-              placeholder="email@mail.com"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
+              type="submit"
+              className={
+                "flex font-semibold px-5 py-3 m-5 mx-auto rounded-lg outline outline-2 outline-white disabled:opacity-25"
+              }
+              disabled={HandleDisabled()}
+              onClick={HandleSubmit}
             />
           </div>
-          <textarea
-            id="message"
-            className={`${inputStyle} ${focusClasses} ${placeholderClasses} pb-28 md:w-full`}
-            placeholder="message"
-            onChange={(e) => {
-              setMessage(e.target.value);
-            }}
-          />
-          <button
-            type="submit"
-            disabled={HandleDisabled()}
-            onClick={HandleSubmit}
-            className={
-              "flex font-semibold px-5 py-3 m-5 mx-auto rounded-lg outline outline-2 outline-white disabled:opacity-25"
-            }
-          >
-            Submit
-          </button>
-        </div>
+        </form>
       ) : (
         <EmailSent firstName={firstName} />
       )}
-    </form>
+    </div>
   );
 };
