48d1954f74ec773ddade1554d961c8c023d66375
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-12-21 21:08:47 -0800

    floating player button update

--- src/components/audio-track-player/index.tsx
+++ src/components/audio-track-player/index.tsx
@@ -1,4 +1,10 @@
-import React, { useState, useRef, Dispatch, SetStateAction } from "react";
+import React, {
+  useState,
+  useRef,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+} from "react";
 
 import { HandlePlayback } from "./playback";
 import ProgressBar from "./progress-bar";
@@ -88,6 +94,62 @@ const AudioTrack = ({
   );
 };
 
+const FloatingPlayPauseButton = ({
+  isPlaying,
+  setIsPlaying,
+  currentTrack,
+  setCurrentTrack,
+}: {
+  isPlaying: boolean;
+  setIsPlaying: Dispatch<SetStateAction<boolean>>;
+  currentTrack: number;
+  setCurrentTrack: Dispatch<SetStateAction<number>>;
+}) => {
+  const [windowDimension, setWindowDimension] = useState(window.innerWidth);
+
+  useEffect(() => {
+    setWindowDimension(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimension(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const isMobile = windowDimension <= 640;
+
+  return (
+    <button
+      className={`transition-opacity duration-200 appear-slow ${
+        currentTrack == -1 && "pointer-events-none opacity-0"
+      } ${
+        isMobile
+          ? "fixed top-4 left-3 w-16 h-16 flex items-center justify-center text-white z-[1]"
+          : "fixed top-4 left-5 z-[1] w-9 h-9"
+      }`}
+      onClick={() =>
+        HandlePlayback({
+          index: currentTrack,
+          currentTrack,
+          setCurrentTrack,
+          isPlaying,
+          setIsPlaying,
+        })
+      }
+    >
+      {isPlaying ? (
+        <img src={Pause} alt="Pause" className="w-full h-full animate-pulse" />
+      ) : (
+        <img src={Play} alt="Play" className="w-full h-full" />
+      )}
+    </button>
+  );
+};
+
 const AudioPlayer = ({ children }: { children: React.ReactNode }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTrack, setCurrentTrack] = useState(-1);
@@ -104,6 +166,9 @@ const AudioPlayer = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div>
+      <FloatingPlayPauseButton
+        {...{ isPlaying, setIsPlaying, currentTrack, setCurrentTrack }}
+      />
       <div className="flex flex-wrap flex-col sm:flex-row sm:max-w-6xl justify-center">
         <div className="flex flex-wrap justify-center">{AudioTracks}</div>
       </div>
